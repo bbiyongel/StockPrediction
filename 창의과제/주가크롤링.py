2c959c4cb9289b24c8e0d1a2c769107ca63fcb8b
juni <hjh4638@naver.com>
2015-10-10 11:23:31 +0900

    날짜 기업코드만 설정하면 알아서 DB 인서트

--- 창의과제/주가크롤링.py
+++ 창의과제/주가크롤링.py
@@ -1,41 +1,39 @@
-# -*- coding: cp949 -*-
-import MySQLdb
-db = MySQLdb.connect("localhost", "root", "rkdqnr123", "stock")
-cursor = db.cursor()
-#with db:
-#   cursor = db.cursor()
-#   sql = '''INSERT INTO stock_price VALUES (%s, %s, %s, %s, %s, %s, %s)'''
-#    cursor.execute(sql, ('test', 'Aug 28, 2015', 0, 1 ,2, 3, 4))
-
 import requests
 import json
 from BeautifulSoup import BeautifulSoup
 from datetime import timedelta, date, datetime
+
+import MySQLdb
+
+# DB 연결
+db = MySQLdb.connect("localhost", "root", "rkdqnr123", "stock")
+cursor = db.cursor()
+
+# 날짜 설정
+end = datetime(2015, 10, 10)
+start = end + delta
+delta = timedelta(days=-200)
+
+# 회사 코드설정
+company = '009450'
+
+# 크롤링에 필요 요소
 url ='https://www.google.com/finance/historical'
 month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 month_in = {'Jan':'1', 'Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
-delta = timedelta(days=-200)
-
-f= open('C:\Users\Juni\Desktop\list.txt', 'w')
-str = 'date\topen\thigh\tlow\tclose\tvolume\n'
-f.write(str)
-f.close()
-
-# 파일 저장 경로
-f= open('C:\Users\Juni\Desktop\list.txt', 'a')
 
 # DB 초기화
-sql = '''DELETE FROM stock_price WHERE stock_id = %s'''%'078020'
+print 'DB reset'
+sql = '''DELETE FROM stock_price WHERE stock_id = %s'''%company
 cursor.execute(sql)
 db.commit()
 
-end = datetime(2015, 8, 30)
-start = end + delta
-for i in range(55):
+# 1300일 정도 받아옴
+for i in range(20):
+		# 구글에 맞는 파라미터 생성
         paramEnd        = month[end.month-1]  +end.strftime(" %d, %Y")
         paramStart      = month[start.month-1]+start.strftime(" %d, %Y")
-        
-        reParam = {'q':'KRX:078020', 'startdate':paramStart, 'enddate':paramEnd, 'num':'200'}
+        reParam = {'q':'KRX:'+company, 'startdate':paramStart, 'enddate':paramEnd, 'num':'200'}
         r =requests.get(url, params=reParam)
 
         soup =BeautifulSoup(r.text)
@@ -46,6 +44,7 @@ for i in range(55):
                 for tr in trs[1:]:
                         stock ={}
                         cols =tr.findAll('td')
+						
                         #날짜 변환
                         date_temp = cols[0].text
                         date_temp = date_temp.replace(',','')
@@ -57,12 +56,9 @@ for i in range(55):
                         stock['low']    =(cols[3].text).replace(',','')
                         stock['close']  =(cols[4].text).replace(',','')
                         stock['volume'] =(cols[5].text).replace(',','')
-#str = stock['date']+'\t'+stock['open']+'\t'+stock['high']+'\t'+stock['low']+'\t'+stock['close']+'\t'+stock['volume']+'\n'
                         
                         sql = '''INSERT INTO stock_price VALUES (%s, %s, %s, %s, %s, %s, %s)'''
-                        cursor.execute(sql, ('078020', stock['date'], stock['open'], stock['high'] , stock['low'], stock['close'], stock['volume']))
-
-                        f.write(str)
+                        cursor.execute(sql, (company, stock['date'], stock['open'], stock['high'] , stock['low'], stock['close'], stock['volume']))
                 end = start
                 start = end+delta
         else:
@@ -71,49 +67,3 @@ for i in range(55):
 db.commit()
 cursor.close()
 db.close()
-f.close()
-
-                
-                
-                
-                
-                
-                
-#fn ='C:\Users\Juni\Desktop\list.json'
-
-#fout = open('C:\Users\Juni\Desktop\list3.txt','w')
-#count = 0
-#with open(fn, 'r') as f:
-#       stock_list =json.load(f)
-#       for s in stock_list[:]:
-#               count+=1
-#               #fprint s['full_code'], s['code'][1:], s['name']        
-#               data = s['code'][1:] +" "
-#               fout.write(data)
-#               data = s['name'].encode('utf-8') +"<br>\n"
-#               fout.write(data)
-                
-#print count
-
-                
-                
-                
-                
-                
-                
-                
-#fn ='C:\Users\Juni\Desktop\list.json'
-
-#fout = open('C:\Users\Juni\Desktop\list3.txt','w')
-#count = 0
-#with open(fn, 'r') as f:
-#       stock_list =json.load(f)
-#       for s in stock_list[:]:
-#               count+=1
-#               #fprint s['full_code'], s['code'][1:], s['name']        
-#               data = s['code'][1:] +" "
-#               fout.write(data)
-#               data = s['name'].encode('utf-8') +"<br>\n"
-#               fout.write(data)
-                
-#print count
